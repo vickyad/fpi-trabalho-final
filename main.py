b7fde36c245654009a29ffccc50ea334b04cebc3
Victoria <victoria.ad40@gmail.com>
2021-04-15 08:56:26 -0300

    Feat (the same thing, but different? )

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def grab_cut(img_target):
     mask = np.zeros(img_target.shape[:2], np.uint8)
     bgdModel = np.zeros((1, 65), np.float64)
     fgdModel = np.zeros((1, 65), np.float64)
-    rect = (50, 50, 450, 290)
+    rect = (0, 0, img_target.shape[0], img_target.shape[1])
     cv.grabCut(img_target, mask, rect, bgdModel, fgdModel, 5, cv.GC_INIT_WITH_RECT)
     mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
     img = img_target * mask2[:, :, np.newaxis]
@@ -15,20 +15,21 @@ def grab_cut(img_target):
 
 
 def poisson_editing(img_src, img_dst):
-    im_mask = np.full(img_dst.shape, 255, dtype=np.uint8)  # isso que nós tem que fazer :D
+    mask = 255 * np.ones(img_dst.shape, img_dst.dtype)
 
-    center = (round(img_src.shape[1] / 2), round(im_src.shape[0] / 2))
+    width, height, channels = im_src.shape
+    center = (round(height / 2), round(width / 2))
 
-    im_clone = cv.seamlessClone(img_dst, img_src, im_mask, center, cv.MIXED_CLONE)
+    mixed_clone = cv.seamlessClone(img_dst, img_src, mask, center, cv.MIXED_CLONE)
 
-    cv.imshow("clone", im_clone)
-    cv.waitKey(0)
+    cv.imwrite("images/opencv-mixed-clone-example.jpg", mixed_clone)
 
 
 if __name__ == '__main__':
     img = cv.imread('dog.jpg')
+    img_cut = cv.imread("images/airplane_cut.jpg")
     im_src = cv.imread("images/airplane.jpg")
     im_dst = cv.imread("images/sky.jpg")
 
-    grab_cut(img)
+    grab_cut(img_cut)
     poisson_editing(im_src, im_dst)
